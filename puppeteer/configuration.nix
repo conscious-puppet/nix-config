49d66ea8b6308ea5d1b221ec8dc10b6d22cf7a73
Abhishek Singh <abhishek17513@gmail.com>
2026-06-09 01:24:05 +0530

    tune spotlight search

--- puppeteer/configuration.nix
+++ puppeteer/configuration.nix
@@ -31,6 +31,37 @@
 
     system.defaults = {
       dock.autohide = true;
+      finder = {
+        _FXShowPosixPathInTitle = true; # show full path in finder title
+        AppleShowAllExtensions = true; # show all file extensions
+        FXEnableExtensionChangeWarning = false; # disable warning when changing file extension
+        # QuitMenuItem = true; # enable quit menu item
+        ShowPathbar = true; # show path bar
+        # ShowStatusBar = true; # show status bar
+      };
+
+      controlcenter.BatteryShowPercentage = true;
+
+      CustomUserPreferences = {
+        "com.apple.Spotlight" = {
+          EnabledPreferenceRules = [
+            "Custom.relatedContents"
+            "com.apple.iBooksX"
+            "com.apple.iCal"
+            "com.apple.AddressBook"
+            "com.apple.Dictionary"
+            "com.apple.mail"
+            "com.apple.Notes"
+            "com.apple.Photos"
+            "com.apple.podcasts"
+            "com.apple.reminders"
+            "com.apple.shortcuts"
+            "com.apple.VoiceMemos"
+            "com.apple.Safari"
+            "com.apple.tips"
+          ];
+        };
+      };
 
     };
     system.keyboard = {
