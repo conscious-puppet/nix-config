2143edb68755f5544d785131115d389c6fd63a62
Abhishek Singh <abhishek17513@gmail.com>
2026-06-09 17:52:43 +0530

    add standalone home-manager config for work (pt 3)

--- work/default.nix
+++ work/default.nix
@@ -3,17 +3,10 @@ let
   username = "abhishek.singh1";
 in
 {
-  perSystem =
-    { self'
-    , inputs'
-    , pkgs
-    , system
-    , config
-    , ...
-    }:
-    {
-      legacyPackages.homeConfigurations."abhishek.singh1" =
-        inputs.home-manager.lib.homeManagerConfiguration {
+  perSystem = { inputs', pkgs, ... }: {
+    legacyPackages.homeConfigurations."abhishek.singh1" =
+      inputs.home-manager.lib.homeManagerConfiguration
+        {
           inherit pkgs;
           modules = [
             ../home
@@ -21,8 +14,11 @@ in
             ./zsh.nix
           ];
           extraSpecialArgs = { inherit inputs username; };
+          sharedModules = [
+            inputs.mac-app-util.homeManagerModules.default
+          ];
         };
 
-      apps.work.program = "${inputs'.home-manager.packages.default}/bin/home-manager";
-    };
+    apps.work.program = "${inputs'.home-manager.packages.default}/bin/home-manager";
+  };
 }
