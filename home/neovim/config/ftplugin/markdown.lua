1f910074c2a66c3977ad963179e83e50224750ca
Abhishek Singh <abhishek17513@gmail.com>
2026-07-17 10:47:41 +0530

    update markdown shiftwidth

--- home/neovim/config/ftplugin/markdown.lua
+++ home/neovim/config/ftplugin/markdown.lua
@@ -1,6 +1,7 @@
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false
 vim.opt_local.signcolumn = "no"
-vim.opt_local.wrap = true
+vim.opt_local.textwidth = 78
+vim.opt_local.wrap = false -- not needed, since text width is 78
 vim.opt_local.conceallevel = 3
 vim.opt_local.spell = true
