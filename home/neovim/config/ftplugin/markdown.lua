e9895d482e51fc76a2f8c2bca3090820004fa487
Abhishek Singh <abhishek17513@gmail.com>
2026-07-17 11:27:43 +0530

    disable conceal in markdown. it breaks long lines with links

--- home/neovim/config/ftplugin/markdown.lua
+++ home/neovim/config/ftplugin/markdown.lua
@@ -3,5 +3,5 @@ vim.opt_local.relativenumber = false
 vim.opt_local.signcolumn = "no"
 vim.opt_local.textwidth = 78
 vim.opt_local.wrap = false -- not needed, since text width is 78
-vim.opt_local.conceallevel = 3
+vim.opt_local.conceallevel = 0 -- this breaks the text
 vim.opt_local.spell = true
